c90b5b9a209efd0c93ddb452be34f87de9450fd0
loc <loc@gmail.com>
2022-03-01 20:40:03 +0700

    add pulic

---
+++ public/bosung.php
@@ -0,0 +1,125 @@
+<!--  MOdal layuot    -->
+<div class="modal">
+    <div class="modal__overlay">
+    </div>
+    <div class="modal__body">
+
+        <!-- auth form-register -->
+
+        <!-- auth form-login-->
+
+    </div>
+</div>
+
+<!-- auth form-register -->
+<div class="auth-form">
+    <div class="auth-form__container">
+        <!-- auth-form__header-->
+        <div class="auth-form__header">
+            <h3 class="auth-form__dk"> Đăng ký</h3>
+            <a href="#" class="auth-form__dn">Đăng nhập</a>
+        </div>
+        <!-- auth-form__body-->
+        <div class="auth-form__body">
+            <div class="auth-form__group">
+                <input type="text" class="form__group-input" placeholder="Tên Đăng Nhập">
+            </div>
+            <div class="auth-form__group">
+                <input type="password" class="form__group-input" placeholder="Mật khẩu">
+            </div>
+            <div class="auth-form__group">
+                <input type="password" class="form__group-input" placeholder="Nhập lại mật khẩu">
+            </div>
+        </div>
+        <!-- RULES-->
+        <div class="auth-form__rules">
+            <p class="form__rules-text">
+                Bằng việc đăng ký bạn đã đồng ý với SHOP LOC về
+                <a href="#" class="rules-text_link">Điều khoản</a> &
+                <a href="#" class="rules-text_link">Dịch vụ</a>
+            </p>
+        </div>
+        <!-- control-->
+        <div class="auth-form__controls">
+            <button class="btn form__controls-back">TRỞ LẠI</button>
+            <button class="btn btn--primary form__controls-dk ">ĐĂNG KÝ</button>
+        </div>
+    </div>
+    <!-- socials -->
+    <div class="auth-form__socials">
+        <a href="#" class="btn btn--with-icon form__socials__fb"">
+        <i class="fab fa-facebook-square form__socials-icon"></i>
+        Kết nối với facebook
+        </a>
+        <a href="" class="btn btn--with-icon form__socials__gg ">
+            <i class="fab fa-google form__socials-icon"></i>
+            Kết nối với google
+        </a>
+    </div>
+
+</div>
+
+<!-- auth form-login-->
+<div class="auth-form">
+    <div class="auth-form__container">
+        <!-- auth-form__header-->
+        <div class="auth-form__header">
+            <h3 class="auth-form__dk"> Đăng Nhập</h3>
+            <a href="#" class="auth-form__dn">Đăng Ký</a>
+        </div>
+        <!-- auth-form__body-->
+        <div class="auth-form__body">
+            <div class="auth-form__group">
+                <input type="text" class="form__group-input" placeholder="Tên Đăng Nhập">
+            </div>
+            <div class="auth-form__group">
+                <input type="password" class="form__group-input" placeholder="Mật khẩu">
+            </div>
+        </div>
+        <!-- auth-form__aside-->
+        <div class="auth-form__aside">
+            <div class="auth-form__help">
+                <a href="#" class="auth-form__help-link help-link-forgot">Quên mật khẩu</a>
+                <span class="auth-form__help-separate"></span>
+                <a href="#" class="auth-form__help-link">Cần trợ giúp ?</a>
+            </div>
+        </div>
+        <!-- control-->
+        <div class="auth-form__controls">
+            <button class="btn form__controls-back">TRỞ LẠI</button>
+            <button class="btn btn--primary form__controls-dk ">ĐĂNG NHẬP</button>
+        </div>
+    </div>
+    <!-- socials -->
+    <div class="auth-form__socials">
+        <a href="#" class="btn btn--with-icon form__socials__fb"">
+        <i class="fab fa-facebook-square form__socials-icon"></i>
+        Kết nối với facebook
+        </a>
+        <a href="" class="btn btn--with-icon form__socials__gg ">
+            <i class="fab fa-google form__socials-icon"></i>
+            Kết nối với google
+        </a>
+    </div>
+
+</div>
+
+
+<nav class="category">
+    <h3 class="category__heading">
+        <i class="fas fa-list"></i>
+        Danh mục
+    </h3>
+
+    <ul class="category-ul">
+        <li class="category-li">
+            <a href="#" class="category-li__link category-li--active">Trang điểm mặt</a>
+        </li>
+        <li class="category-li">
+            <a href="#" class="category-li__link">Trang điểm môi</a>
+        </li>
+        <li class="category-li">
+            <a href="#" class="category-li__link">Trang điểm mắt</a>
+        </li>
+    </ul>
+</nav>
---
+++ public/test.php
@@ -0,0 +1,34 @@
+<center>
+<h1 class="mobile">mobile</h1>
+<h1 class="tablet">tablet</h1>
+<h1 class="pc">pc</h1>
+</center>
+<style>
+    .mobile,.tablet,.pc {
+        display: none;
+    }
+    /*RESPONSIVE*/
+
+    /*RESPONSIVE*/
+
+    /*mobile : width <= 739px (46.25em) */
+    @media only screen and (max-width: 46.1875em) {
+
+    }
+
+    /*Tablet: width >= 740px(46.25em) and width <= 1023px (63.9375em) */
+    @media only screen and (min-width: 46.25em) and (max-width: 63.9375em) {
+
+    }
+
+    /*Mobile & tablet : width =< 1023px (63.9375em) */
+    @media screen and (max-width: 63.9375em) {
+
+    }
+
+    /*PC : width >= 1024px (64em) */
+    @media screen and (min-width: 64em) {
+
+    }
+</style>
+<p>hhihi</p>
